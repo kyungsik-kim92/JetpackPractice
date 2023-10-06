ab0c7faf2151ea81ad17d9ffd3b506e07a095059
Kim Kyungsik <ksiki1@naver.com>
2023-10-06 18:49:43 +0900

    바인딩어댑터 기초.

---
+++ databindingpractice/src/main/java/com/example/databindingpractice/BindingAdapter.kt
@@ -0,0 +1,26 @@
+package com.example.databindingpractice
+
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+
+
+@BindingAdapter("myAge")
+fun myAgeSetting(view : TextView, age : Int) {
+
+    if(age > 20) {
+        view.text= age.toString() + "보다 많음"
+    } else {
+        view.text= age.toString()
+    }
+
+}
+
+@BindingAdapter("myImg")
+fun myImageSetting(view : ImageView, age : Int) {
+
+    if(age > 20) {
+        view.setImageResource(R.drawable.ic_launcher_foreground)
+    }
+
+}
